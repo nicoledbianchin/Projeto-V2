31559a2e31699dde2d8d00c4e6734ad21499cdbd
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-18 16:43:47 -0300

    refatora nome de classe e adiciona método no repositorio

--- src/main/java/com/demo/AcessingDataJpaApplication.java
+++ src/main/java/com/demo/AcessingDataJpaApplication.java
@@ -1,13 +1,18 @@
 package com.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	private static final Logger log = LoggerFactory.getLogger()
 
+	public static void main(String[] args) {
+
+		SpringApplication.run(Accessing);
+	}
 }
