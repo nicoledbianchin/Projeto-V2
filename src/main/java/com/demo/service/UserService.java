f16ed0ed52bcf25cb566a181d5b31d58697bdfd7
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-20 16:30:18 -0300

    refatora métodos do user service

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -2,24 +2,27 @@ package com.demo.service;
 
 import com.demo.domain.User;
 import com.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    private UserRepository repository;
+    private UserRepository userRepository;
 
-    private List<User> findAll(){
-        return (List<User>) repository.findAll();
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    private List<User> findAll(UserRepository r){
+        return (List<User>) userRepository.findAll();
     }
 
     private List<User> findByName(String name){
-        return repository.findByName(name);
+        return userRepository.findByName(name);
     }
 
     private User findById(long id){
-        return repository.findById(id);
+        return userRepository.findById(id);
     }
 }
