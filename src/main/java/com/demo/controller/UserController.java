47a0803d1e285d67793f01b0c2fece571bac9d50
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-20 16:18:08 -0300

    adiciona dependencias do spring mvc

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.demo.controller;
 
 import com.demo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
