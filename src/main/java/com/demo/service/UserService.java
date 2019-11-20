eba1b8e23316a8590581de0fa8d8e8ba7750aba8
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-20 16:36:38 -0300

    implementa método save

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
     private User findById(long id){
         return userRepository.findById(id);
     }
+
+    public User save(User user){
+        return userRepository.save(user);
+    }
 }
