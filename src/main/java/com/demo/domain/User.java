fc23e916cf9f12af398af0dfd0426573fd600423
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-23 20:42:17 -0300

    invoca construtor que tem id nos parametros

--- src/main/java/com/demo/domain/User.java
+++ src/main/java/com/demo/domain/User.java
@@ -15,13 +15,18 @@ public class User {
     private String email;
     private String password;
 
+    public User(){
+    }
+
     public User (String name, String email, String password){
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public User(){
+    public User(String name, String email, String password, Long id){
+        this(name, email, password);
+        this.id = id;
     }
 
     public Long getId() {
