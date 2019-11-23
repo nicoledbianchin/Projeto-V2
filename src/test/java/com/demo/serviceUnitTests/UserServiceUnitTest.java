b34645e3f1483bbde4b5d96fa824d23566c7af5b
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-23 14:13:56 -0300

    implementa teste unitario do user service salvando usuario valido

--- src/test/java/com/demo/serviceUnitTests/UserServiceUnitTest.java
+++ src/test/java/com/demo/serviceUnitTests/UserServiceUnitTest.java
@@ -1,10 +1,14 @@
 package com.demo.serviceUnitTests;
 
+import com.demo.domain.User;
 import com.demo.repository.UserRepository;
 import com.demo.service.UserService;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -15,4 +19,17 @@ public class UserServiceUnitTest {
 
     @InjectMocks
     UserService userService;
+
+    @Test
+    public void shouldReturnOkWhenSaveValidUser(){
+        User user = new User("name", "name@email.com", "password");
+        UserService userService = new UserService(userRepository);
+
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        User userExpected = userService.save(user);
+
+        Assert.assertEquals(user.getName(), userExpected.getName());
+        Assert.assertEquals(user.getEmail(), userExpected.getEmail());
+        Assert.assertEquals(user.getPassword(), userExpected.getPassword());
+    }
 }
